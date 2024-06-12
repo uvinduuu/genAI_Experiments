ff39678d2cec0c94a3e765dd0eba417cee0993ac
Uvindu Kodikara <153884375+uvinduuu@users.noreply.github.com>
2024-06-12 11:14:33 +0530

    Update openAI.py

--- openAI.py
+++ openAI.py
@@ -5,19 +5,42 @@ os.environ['OPENAI_API_KEY'] = openai_key
 from langchain.llms import OpenAI
 
 llm = OpenAI(temperature = 0.6) #how creative the llm is
-name = llm("I want to open an Indian restaurent, suggest me a name for that.")
+name = llm("I want to open an Indian restaurant, suggest me a name for that.")
 print(name)
 
 from langchain.prompts import PromptTemplate
 
 prompt_template_name = PromptTemplate(
   input_variables = ['cuisine'],
-  template - "I want to open an {cuisine} restaurent, suggest me a name for that' 
+  template - "I want to open an {cuisine} restaurant, suggest me a name for that" 
+)
+name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="restaurant_name")
+
+'''
+prompt_template_name.format(cuisine="Italian")
+from langchain.chains import LLMChain
+name_chain.run("Mexican")
+'''
+
+prompt_template_items = PromptTemplate(
+  input_variables = ['restaurant_name'],
+  template - "Suggest some menu items for {restaurant_name}. Return it as a comma seperatead list." 
+)
+food_items_chain = LLMChain(llm=llm, prompt=prompt_template_items, output_key="menu_items")
+
+#for one output
+from langchain.chains import SimpleSequentialChain
+
+chain = SimpleSequentialChain(chains = [name_chain, food_items_chain])
+print(response)
+
+#multiple outputs
+from langchain.chains import SequentialChain
+
+chain = SequentialChain(
+  chains = [name_chain, food_item_chain],
+  input_variables = ['cuisine'],
+  output_variables = ['restaurant_name' , 'menu_items']
 )
 
-prompt_template_name.format(cuisine="Italian")
-
-from langchain.chains import LLMChain
-
-chain = LLMChain(llm=llm, prompt=prompt_template_name)
-chain.run("Mexican")
+chain({'cuisine' : 'Arabic})
